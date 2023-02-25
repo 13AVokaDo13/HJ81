fcaa2045a686e40c799e7ba1222491b9363e867f
Sofin Rodion <13avokado13@gmail.com>
2023-02-25 11:43:47 +0300

    init commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,14 @@
+package org.example;
+
+    public class Post {
+        public String name;
+        public String passport;
+        public String patronymic;
+        public String phone;
+        public String surname;
+        public boolean subscription;
+        public FormDate birthday = new FormDate();
+        public FormDate month = new FormDate();
+        public FormDate year = new FormDate();
+    }
+
